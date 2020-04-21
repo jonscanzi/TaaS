5fabab895eda40481c959e579b94e3f38dc98c25
Jonathan Scanzi <kratosjon@gmail.com>
2020-04-21 12:21:48 +0000

    Add user-override for webserver VM type in config and with env variable

--- orchestrator/src/paths.rs
+++ orchestrator/src/paths.rs
@@ -4,6 +4,7 @@ pub const PROVIDER: &str = "config/provider.yml";
 pub const COMMON_OS: &str = "config/common_os.yml";
 pub const DEFAULT_VALUES: &str = "config/default_values.yml";
 pub const PROVIDERS_CONFIG: &str = "config/providers_config.yml";
+pub const WS_CONFIG: &str = "config/webserver.yml";
 pub const NETWORK_CONFIG: &str = "config/network.yml";
 pub const SSH_CONFIG: &str = "config/ssh.yml";
 pub const DEPLOYMENT_TEMPLATES_PATH: &str = "deployment_templates";
